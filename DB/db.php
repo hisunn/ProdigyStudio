692396ecd2709b041549c8e0ac8c67ac700beb2f
hisunn <66265180+hisunn@users.noreply.github.com>
2022-07-13 18:31:40 +0800

    changes db connection

--- DB/db.php
+++ DB/db.php
@@ -1,14 +1,15 @@
 <?php
 
 $servername = "localhost";
-$uname = "root";
-$password = "";
-$db = "prodigystudio";
+$uname = "u714367839_ihsan";
+$password = "Abc123456789";
+$db = "u714367839_prodigystudio";
 
 // Create connection
 $conn = new mysqli($servername, $uname, $password, $db);
 
+
 // Check connection
-if ($conn->connect_error) {
-  header('location:/error.php');
-}
+// if ($conn->connect_error) {
+//   header('location:/error.php');
+// }
